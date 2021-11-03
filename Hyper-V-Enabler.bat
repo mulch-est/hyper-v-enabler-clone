e0270d125f5ac294c83e3fdb7d9c0f128358d58c
mulch <52766118+mulch-est@users.noreply.github.com>
2021-11-03 09:27:18 -0400

    Update Hyper-V-Enabler.bat

--- Hyper-V-Enabler.bat
+++ Hyper-V-Enabler.bat
@@ -1,3 +1,5 @@
+:: Source: https://www.how2shout.com/how-to/download-and-install-hyper-v-on-windows-10-home.html
+:: Working as of 1 Nov 2021
 pushd "%~dp0"
 dir /b %SystemRoot%\servicing\Packages\*Hyper-V*.mum >hyper-v.txt
 for /f %%i in ('findstr /i . hyper-v.txt 2^>nul') do dism /online /norestart /add-package:"%SystemRoot%\servicing\Packages\%%i"
